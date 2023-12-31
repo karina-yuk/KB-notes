98f94e9039e441eaa57d51336380b0dda81caab4
Karina Li <karina.yukting.li@gmail.com>
2023-12-30 19:52:52 -0500

    Imported html and api routes to server.js

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const htmlRoutes = require("./routes/htmlRoutes"); 
+const apiRoutes = require("./routes/apiRoutes"); 
+
 app.use(express.static("public"));
 app.use(express.json());
+
+app.use("/api", apiRoutes); 
+app.use("/", htmlRoutes); 
 
 app.listen(PORT, () =>
   console.log(`Server listening on http://localhost:${PORT}`)
